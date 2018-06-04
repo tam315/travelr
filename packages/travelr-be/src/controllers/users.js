c06aa1afcb571acd929842f042f0a9f4f42fdcfb
stam <junkboy0315@gmail.com>
2018-06-04 17:35:36 +0900

    GET /users/{users.id}

--- packages/travelr-be/src/controllers/users.js
+++ packages/travelr-be/src/controllers/users.js
@@ -1,13 +1,24 @@
-const pool = require('../helper/db').createPool();
+const dbHelper = require('../helper/db');
 
+const db = dbHelper.db;
+
+// TODO: authorization
 exports.getUser = async (req, res, next) => {
   const { userId } = req.params;
-  const user = await pool.query(
-    'SELECT * FROM users WHERE id = ? LIMIT 1',
+
+  const user = await db.oneOrNone(
+    'SELECT * FROM get_user WHERE id = $1 LIMIT 1',
     userId,
   );
+
+  if (!user) return res.status(421).end();
+
   res.status(200).send({
-    userId: user[0].id,
-    displayName: user[0].display_name,
+    userId: user.id,
+    displayName: user.display_name,
+    isAdmin: user.is_admin,
+    earnedViews: +user.earned_views,
+    earnedLikes: +user.earned_likes,
+    earnedComments: +user.earned_comments,
   });
 };
