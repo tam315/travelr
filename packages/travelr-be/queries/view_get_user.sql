eb22f44e9b7860b95234e302a0ea137ea9c77b59
stam <junkboy0315@gmail.com>
2018-06-04 19:22:33 +0900

    use space not tab

--- packages/travelr-be/queries/view_get_user.sql
+++ packages/travelr-be/queries/view_get_user.sql
@@ -3,40 +3,40 @@ DROP VIEW IF EXISTS get_user;
 CREATE VIEW get_user AS
 
 SELECT
-	users.*,
-	stats.earned_views,
-	stats.earned_likes,
-	stats.earned_comments
+  users.*,
+  stats.earned_views,
+  stats.earned_likes,
+  stats.earned_comments
 FROM users
 
 -- join stats data for each user
 LEFT OUTER JOIN (
-	SELECT
-		posts.user_id,
-		sum(posts.view_count) AS earned_views,
-		sum(stats_likes.earned_likes) as earned_likes,
-		sum(stats_comments.earned_comments) as earned_comments
-	FROM posts
+  SELECT
+    posts.user_id,
+    sum(posts.view_count) AS earned_views,
+    sum(stats_likes.earned_likes) as earned_likes,
+    sum(stats_comments.earned_comments) as earned_comments
+  FROM posts
 
-	-- join total 'likes' count for each post
-	LEFT OUTER JOIN (
-		SELECT likes.post_id, count(*) AS earned_likes
-		FROM likes
-		GROUP BY post_id
-	)
-	AS stats_likes
-	ON (posts.id = stats_likes.post_id)
+  -- join total 'likes' count for each post
+  LEFT OUTER JOIN (
+    SELECT likes.post_id, count(*) AS earned_likes
+    FROM likes
+    GROUP BY post_id
+  )
+  AS stats_likes
+  ON (posts.id = stats_likes.post_id)
 
-	-- join total 'comments' count for each post
-	LEFT OUTER JOIN (
-			SELECT comments.post_id, count(*) AS earned_comments
-		FROM comments
-		GROUP BY post_id
-	)
-	AS stats_comments
-	ON (posts.id = stats_comments.post_id)
+  -- join total 'comments' count for each post
+  LEFT OUTER JOIN (
+      SELECT comments.post_id, count(*) AS earned_comments
+    FROM comments
+    GROUP BY post_id
+  )
+  AS stats_comments
+  ON (posts.id = stats_comments.post_id)
 
-	GROUP BY posts.user_id
+  GROUP BY posts.user_id
 )
 AS stats
 ON users.id = stats.user_id;
