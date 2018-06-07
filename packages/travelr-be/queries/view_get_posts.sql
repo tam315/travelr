d196eddd39cc7bc58450053afec5b7b1d2dc74f1
stam <junkboy0315@gmail.com>
2018-06-08 08:25:28 +0900

    simplify query

--- packages/travelr-be/queries/view_get_posts.sql
+++ packages/travelr-be/queries/view_get_posts.sql
@@ -3,16 +3,16 @@ DROP VIEW IF EXISTS get_posts;
 CREATE VIEW get_posts AS
 
 WITH
-  -- calculate likes counts for each post_id
-  stats_likes AS (
-    SELECT post_id, count(*) AS liked_count
+  -- total likes count for each post
+  posts_likes AS (
+    SELECT post_id, count(*)
     FROM likes
     GROUP BY post_id
   ),
 
-  -- calculate comments counts for each post_id
-  stats_comments AS (
-    SELECT post_id, count(*) AS comments_count
+  -- total comments count for each post
+  posts_comments AS (
+    SELECT post_id, count(*)
     FROM comments
     GROUP BY post_id
   )
@@ -22,9 +22,9 @@ SELECT
   ST_X(geom) AS lng,
   ST_Y(geom) AS lat,
   users.display_name,
-  stats_likes.liked_count,
-  stats_comments.comments_count
+  posts_likes.count AS liked_count,
+  posts_comments.count AS comments_count
 FROM posts
 LEFT OUTER JOIN users ON (posts.user_id = users.id)
-LEFT OUTER JOIN stats_likes ON (posts.id = stats_likes.post_id)
-LEFT OUTER JOIN stats_comments ON (posts.id = stats_comments.post_id)
+LEFT OUTER JOIN posts_likes ON (posts.id = posts_likes.post_id)
+LEFT OUTER JOIN posts_comments ON (posts.id = posts_comments.post_id)
--- packages/travelr-be/queries/view_get_user.sql
+++ packages/travelr-be/queries/view_get_user.sql
@@ -3,14 +3,14 @@ DROP VIEW IF EXISTS get_user;
 CREATE VIEW get_user AS
 
 WITH
-  -- calculate total earned likes count for each post
+  -- total likes count for each post
   posts_likes AS (
     SELECT post_id, count(*)
     FROM likes
     GROUP BY post_id
   ),
 
-  -- calculate total earned comments count for each post
+  -- total comments count for each post
   posts_comments AS (
     SELECT post_id, count(*)
     FROM comments
@@ -21,14 +21,14 @@ WITH
   -- then, group by user_id and get stats for each user.
   stats AS (
     SELECT
-      posts.user_id,
+      user_id,
       sum(posts.view_count) AS earned_views,
       sum(posts_likes.count) as earned_likes,
       sum(posts_comments.count) as earned_comments
     FROM posts
     LEFT OUTER JOIN posts_likes ON (posts.id = posts_likes.post_id)
     LEFT OUTER JOIN posts_comments ON (posts.id = posts_comments.post_id)
-    GROUP BY posts.user_id
+    GROUP BY user_id
   )
 
 SELECT
