3d5d6d76beb4970daa0c7d2e577450e70c73a4d9
stam <junkboy0315@gmail.com>
2018-06-22 11:43:03 +0900

    add test

---
+++ packages/travelr-fe/src/components/__tests__/PageViewPostsMap.test.jsx
@@ -0,0 +1,45 @@
+import { mount } from 'enzyme';
+import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import PageViewPostsMap from '../PageViewPostsMap';
+import MapsHelper from '../../utils/MapsHelper';
+
+jest.mock('../../utils/MapsHelper');
+
+const DUMMY_POSTS = [
+  { postId: 1, likedCount: 888 },
+  { postId: 2, likedCount: 999 },
+];
+
+const DUMMY_POSTS_UPDATED = [
+  { postId: 3, likedCount: 888 },
+  { postId: 4, likedCount: 999 },
+];
+
+let wrapper;
+let mockPlacePostsFunc;
+
+describe('PageViewPostsMap component', () => {
+  beforeAll(() => {
+    // manual implementation is required for the ES6 classes that uses arrow function.
+    // https://facebook.github.io/jest/docs/en/es6-class-mocks.html
+    mockPlacePostsFunc = jest.fn();
+    MapsHelper.prototype.placePosts = mockPlacePostsFunc;
+
+    wrapper = mount(
+      <BrowserRouter>
+        <PageViewPostsMap posts={DUMMY_POSTS} classes={{}} />
+      </BrowserRouter>,
+    );
+  });
+
+  test('instantiate google maps', () => {
+    expect(MapsHelper).toHaveBeenCalledTimes(1);
+  });
+
+  test('re-render posts when posts are updated', () => {
+    expect(mockPlacePostsFunc).toHaveBeenCalledTimes(0);
+    wrapper.setProps({ posts: DUMMY_POSTS_UPDATED, classes: {} });
+    expect(mockPlacePostsFunc).toHaveBeenCalledTimes(1);
+  });
+});
