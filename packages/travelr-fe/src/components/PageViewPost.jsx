da355e8d3c67da4a56a7c26ea01bf2d721c564ed
stam <junkboy0315@gmail.com>
2018-06-26 10:04:09 +0900

    add PageViewPost

--- packages/travelr-fe/src/components/PageViewPost.jsx
+++ packages/travelr-fe/src/components/PageViewPost.jsx
@@ -1,3 +1,175 @@
+import Button from '@material-ui/core/Button';
+import Input from '@material-ui/core/Input';
+import { withStyles } from '@material-ui/core/styles';
+import Typography from '@material-ui/core/Typography';
+import PropTypes from 'prop-types';
 import React from 'react';
+import ReactCompareImage from 'react-compare-image';
+import config from '../config';
+import StatusBadge from './StatusBadge';
 
-export default () => <div>PageViewPost</div>;
+const propTypes = {
+  classes: PropTypes.object.isRequired,
+  match: PropTypes.object.isRequired,
+  token: PropTypes.string,
+};
+
+const defaultProps = { token: null };
+
+const styles = theme => ({
+  root: {
+    maxWidth: 500,
+    margin: 'auto',
+  },
+  container: {
+    display: 'grid',
+    gridGap: `${theme.spacing.unit * 3}px`,
+    marginTop: `${theme.spacing.unit * 4}px`,
+    paddingRight: theme.spacing.unit * 2,
+    paddingLeft: theme.spacing.unit * 2,
+    paddingBottom: theme.spacing.unit * 4,
+  },
+  badges: {
+    display: 'grid',
+    gridGap: `${theme.spacing.unit * 2}px`,
+    gridTemplateColumns: '1fr 1fr 1fr',
+  },
+});
+
+export class PageViewPost extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.postId = this.props.match.params.postId;
+    this.state = {
+      post: {},
+      comment: '',
+    };
+  }
+
+  componentDidMount = () => {
+    this.fetchPost(this.postId); // TODO: pass userId as 2nd arg
+  };
+
+  fetchPost = async postId => {
+    try {
+      const response = await fetch(`${config.apiUrl}posts/${postId}`);
+
+      if (!response.ok) {
+        // TODO: toast
+        return;
+      }
+
+      const post = await response.json();
+      this.setState({ post });
+    } catch (err) {
+      // TODO: toast
+    }
+  };
+
+  createComment = async (postId, comment) => {
+    try {
+      const response = await fetch(`${config.apiUrl}posts/${postId}/comments`, {
+        method: 'POST',
+        headers: { authorization: this.props.token }, // TODO: replace token
+        body: JSON.stringify({ comment }),
+      });
+
+      if (!response.ok) {
+        // TODO: toast
+        return;
+      }
+
+      await this.fetchPost(this.postId);
+    } catch (err) {
+      // TODO: toast
+    }
+  };
+
+  handleChange(e, name) {
+    this.setState({ [name]: e.target.value });
+  }
+
+  renderComments = comments =>
+    comments.map(item => (
+      <div key={item.commentId} className="comment">
+        <Typography variant="body2">{item.displayName}</Typography>
+        <Typography>{item.comment}</Typography>
+        <Typography variant="caption">
+          {new Date(item.datetime).toISOString().substr(0, 10)}
+        </Typography>
+        {/* TODO: add comment edit & delete button */}
+      </div>
+    ));
+
+  render() {
+    const { classes } = this.props;
+    const {
+      oldImageUrl,
+      newImageUrl,
+      description,
+      shootDate,
+      lng,
+      lat,
+      viewCount,
+      displayName,
+      likedCount,
+      commentsCount,
+      comments,
+    } = this.state.post;
+
+    // return nothing if the data is not fetched yet
+    if (!oldImageUrl) return <div />;
+
+    return (
+      <div className={classes.root}>
+        <ReactCompareImage leftImage={oldImageUrl} rightImage={newImageUrl} />
+
+        <div className={classes.container}>
+          <div className={classes.badges}>
+            <StatusBadge icon="like" count={likedCount} />
+            <StatusBadge icon="comment" count={commentsCount} />
+            <StatusBadge icon="view" count={viewCount} />
+          </div>
+
+          <div>
+            <Typography variant="body2">{displayName}</Typography>
+            <Typography>{description}</Typography>
+          </div>
+          <Typography>
+            撮影日：{new Date(shootDate).toISOString().substr(0, 10)}
+          </Typography>
+
+          <div style={{ width: '100%', height: '150px', background: 'gray' }}>
+            google maps goes here
+          </div>
+
+          <Input
+            placeholder="コメントを書く"
+            value={this.state.comment}
+            onChange={e => this.handleChange(e, 'comment')}
+          />
+          {this.state.comment && (
+            <Button
+              onClick={() =>
+                this.createComment(this.postId, this.state.comment)
+              }
+              color="primary"
+              size="large"
+              variant="contained"
+            >
+              コメントする
+            </Button>
+          )}
+
+          {this.renderComments(comments)}
+        </div>
+      </div>
+    );
+  }
+}
+
+PageViewPost.propTypes = propTypes;
+PageViewPost.defaultProps = defaultProps;
+
+export default withStyles(styles)(PageViewPost);
---
+++ packages/travelr-fe/src/components/__tests__/PageViewPost.test.jsx
@@ -0,0 +1,125 @@
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import { shallow } from 'enzyme';
+import React from 'react';
+import ReactCompareImage from 'react-compare-image';
+import { PageViewPost } from '../PageViewPost';
+import StatusBadge from '../StatusBadge';
+
+jest.mock('../StatusBadge');
+
+// url params
+const match = { params: { postId: 12345 } };
+
+// dummy data from the API
+const dummyPost = {
+  oldImageUrl: 'dummy_oldImageUrl',
+  newImageUrl: 'dummy_newImageUrl',
+  description: 'dummy_description',
+  shootDate: '1999-09-09',
+  lng: 'dummy_lng',
+  lat: 'dummy_lat',
+  viewCount: 'dummy_viewCount',
+  displayName: 'dummy_displayName',
+  likedCount: 'dummy_likedCount',
+  commentsCount: 'dummy_commentsCount',
+  comments: [
+    {
+      commentId: 123,
+      userId: 456,
+      datetime: new Date('1985-03-31'),
+      comment: 'dummy_comment',
+    },
+  ],
+};
+
+describe('PageViewPost component', () => {
+  let wrapper;
+
+  beforeEach(() => {
+    fetch.resetMocks();
+
+    wrapper = shallow(<PageViewPost classes={{}} match={match} />);
+    wrapper.setState({
+      post: dummyPost,
+    });
+  });
+
+  test('fetch post when componentDidMount', () => {
+    expect(fetch).toBeCalledTimes(1);
+    expect(fetch.mock.calls[0][0]).toContain(`/posts/${match.params.postId}`);
+  });
+
+  test('render necessary parts', () => {
+    // images
+    expect(wrapper.find(ReactCompareImage)).toHaveLength(1);
+    // badges
+    expect(wrapper.find(StatusBadge)).toHaveLength(3);
+    // displayName
+    expect(
+      wrapper
+        .find(Typography)
+        .at(0)
+        .html(),
+    ).toContain(dummyPost.displayName);
+    // description
+    expect(
+      wrapper
+        .find(Typography)
+        .at(1)
+        .html(),
+    ).toContain(dummyPost.description);
+    // shootDate
+    expect(
+      wrapper
+        .find(Typography)
+        .at(2)
+        .html(),
+    ).toContain(dummyPost.shootDate);
+    // comment writing form
+    expect(wrapper.find({ placeholder: 'コメントを書く' })).toHaveLength(1);
+    // comments
+    expect(wrapper.find({ className: 'comment' }).html()).toContain(
+      dummyPost.comments[0].comment,
+    );
+  });
+
+  test('shows comment posting button when user writes the comment', () => {
+    // button is hidden
+    expect(wrapper.find(Button)).toHaveLength(0);
+
+    // if user write a comment
+    wrapper
+      .find({ placeholder: 'コメントを書く' })
+      .simulate('change', { target: { value: 'cat' } });
+
+    // button is now visible
+    expect(wrapper.find(Button)).toHaveLength(1);
+  });
+
+  test('sends a comment and fetches post if success', done => {
+    fetch.mockResponse();
+
+    // user write a comment
+    wrapper
+      .find({ placeholder: 'コメントを書く' })
+      .simulate('change', { target: { value: 'cat' } });
+
+    // user pressed send button
+    wrapper.find(Button).simulate('click');
+
+    setImmediate(() => {
+      expect(fetch).toHaveBeenCalledTimes(3);
+
+      // 1st time => initial fetchPost
+      // 2nd time => post comment
+      expect(fetch.mock.calls[1][0]).toContain(
+        `/posts/${match.params.postId}/comments`,
+      );
+      expect(fetch.mock.calls[1][1].method).toBe('POST');
+      // 3rd time => fetchPost
+      expect(fetch.mock.calls[2][0]).toContain(`/posts/${match.params.postId}`);
+      done();
+    });
+  });
+});
