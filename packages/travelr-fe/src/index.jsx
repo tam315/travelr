47289ea1dbc0e7d0ac2c61eaa2ebf5d1cca090d5
stam <junkboy0315@gmail.com>
2018-07-15 23:33:08 +0900

    initial redux-observable setup

--- packages/travelr-fe/src/index.jsx
+++ packages/travelr-fe/src/index.jsx
@@ -4,15 +4,21 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { applyMiddleware, createStore } from 'redux';
 import reduxThunk from 'redux-thunk';
+import { createEpicMiddleware } from 'redux-observable';
+import rootEpic from './epics';
 
 import App from './containers/App';
 import reducer from './reducers';
 
-const enhancer = applyMiddleware(reduxThunk);
+const epicMiddleware = createEpicMiddleware();
+
+const enhancer = applyMiddleware(reduxThunk, epicMiddleware);
 
 // don't write initial state here. write it in each reducer.
 // $FlowIgnore
 const store = createStore(reducer, {}, enhancer);
+
+epicMiddleware.run(rootEpic);
 
 ReactDOM.render(
   <Provider store={store}>
