3bd212e444badebaf135b4894da7e736868362dc
stam <junkboy0315@gmail.com>
2018-07-26 11:03:42 +0900

    use babel-plugin-import to optimize imports

--- packages/travelr-fe/.babelrc.js
+++ packages/travelr-fe/.babelrc.js
@@ -5,4 +5,30 @@ module.exports = {
     '@babel/preset-stage-3', // for class-properties & object-rest-spread
     '@babel/preset-typescript',
   ],
+  plugins: [
+    // import optimization
+    //
+    // e.g.)
+    // import { SomeParts } from 'some-entire-library'
+    // ↓
+    // import Someparts from 'some-entire-library/Someparts'
+    [
+      'import',
+      {
+        libraryName: 'lodash',
+        libraryDirectory: '', // default: lib
+        camel2DashComponentName: false, // default: true
+      },
+      'optimize-lodash', // anything some unique
+    ],
+    [
+      'import',
+      {
+        libraryName: '@material-ui/core',
+        libraryDirectory: '',
+        camel2DashComponentName: false,
+      },
+      'optimize-@material-ui/core',
+    ],
+  ],
 };
--- packages/travelr-fe/src/utils/general.ts
+++ packages/travelr-fe/src/utils/general.ts
@@ -1,4 +1,4 @@
-import * as _ from 'lodash';
+import { transform, isEqual, isObject } from 'lodash';
 
 /**
  * Deep diff between two object, using lodash
@@ -6,13 +6,12 @@ import * as _ from 'lodash';
  * @param  {Object} base   Object to compare with
  * @return {Object}        Return a new object who represent the diff
  */
-
 export const difference = (object, base) => {
   const changes = (object, base) =>
-    _.transform(object, (result, value, key) => {
-      if (!_.isEqual(value, base[key])) {
+    transform(object, (result, value, key) => {
+      if (!isEqual(value, base[key])) {
         result[key] =
-          _.isObject(value) && _.isObject(base[key])
+          isObject(value) && isObject(base[key])
             ? changes(value, base[key])
             : value;
       }
