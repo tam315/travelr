67baa17f0d0fd7bf5d6a4fb86779db58281dae22
stam <junkboy0315@gmail.com>
2018-07-30 11:17:40 +0900

    cloud function adapted to node 8

--- packages/travelr-firebase/functions/index.js
+++ packages/travelr-firebase/functions/index.js
@@ -10,174 +10,170 @@ const { spawn } = require('child-process-promise');
 
 const visionClient = new vision.ImageAnnotatorClient();
 
-// Blurs the given file using ImageMagick.
-function blurImage(file) {
-  const tempLocalFilename = path.join(os.tmpdir(), path.basename(file.name));
+// Blurs the given image using ImageMagick.
+const blurImage = async file => {
+  try {
+    // download the image
+    const tempLocalFilename = path.join(os.tmpdir(), path.basename(file.name));
+    await file.download({ destination: tempLocalFilename });
 
-  return file
-    .download({ destination: tempLocalFilename })
-    .then(() =>
-      spawn('convert', [
-        tempLocalFilename,
-        '-type',
-        'GrayScale',
-        '-blur',
-        '0x48',
-        tempLocalFilename
-      ])
-    )
-    .then(() =>
-      file.bucket.upload(tempLocalFilename, {
-        destination: `sanitized/${path.basename(file.name)}`
-      })
-    )
-    .then(() => {
-      console.log(`Blurred image has been uploaded to ${file.name}.`);
+    // blur the image
+    await spawn('convert', [
+      tempLocalFilename,
+      '-type',
+      'GrayScale',
+      '-blur',
+      '0x48',
+      tempLocalFilename
+    ]);
 
-      return { sanitized: true };
-    })
-    .catch(err => console.error('bluring image failed: ', err));
-}
+    // upload the image to the `/sanitized/` folder
+    await file.bucket.upload(tempLocalFilename, {
+      destination: `sanitized/${path.basename(file.name)}`
+    });
+
+    console.log(`Blurred image has been uploaded to ${file.name}.`);
+    return { sanitized: true };
+  } catch (err) {
+    console.error('bluring image failed: ', err);
+    return false;
+  }
+};
 
 // Blurs uploaded images that are flagged as Adult or Violence.
 // https://cloud.google.com/vision/docs/detecting-safe-search
-function blurOffensiveImages(object) {
-  const file = storage.bucket(object.bucket).file(object.name);
-  const bucketName = object.bucket;
-  const fileName = object.name;
-
-  if (fileName.startsWith('sanitized/')) {
-    console.log('skipping sanitization as already sanitized: ', fileName);
-    return Promise.resolve({ sanitized: false });
-  }
-
-  console.log(`Analyzing ${fileName}.`);
-
-  return visionClient
-    .safeSearchDetection(`gs://${bucketName}/${fileName}`)
-    .then(([safeSearch]) => {
-      if (
-        safeSearch.safeSearchAnnotation.adult === 'VERY_LIKELY' ||
-        safeSearch.safeSearchAnnotation.violence === 'VERY_LIKELY'
-      ) {
-        console.log(
-          `The image ${file.name} has been detected as inappropriate.`
-        );
-        return blurImage(file);
-      }
-
-      console.log(`The image ${fileName} has been detected as OK.`);
-      return { sanitized: false };
-    })
-    .catch(err => console.error('failed image analyzation: ', err));
-}
-
-exports.onFileChange = functions.storage
-  .bucket('travelr-images')
-  .object()
-  .onFinalize(object => {
+const blurOffensiveImages = async object => {
+  try {
+    const file = storage.bucket(object.bucket).file(object.name);
     const bucketName = object.bucket;
     const fileName = object.name;
 
-    console.log('File upload detected!');
-
-    if (
-      fileName.startsWith('1024w/') ||
-      fileName.startsWith('192w/') ||
-      fileName.startsWith('96w/')
-    ) {
-      console.log('Ignoreing already resized files!', fileName);
-      return true;
+    // do nothing for already sanitized image
+    if (fileName.startsWith('sanitized/')) {
+      console.log('skipping sanitization as already sanitized: ', fileName);
+      return { sanitized: false };
     }
 
-    return blurOffensiveImages(object)
-      .then(({ sanitized }) => {
-        // do nothing if the image was sanitized.
-        // as the new image will be uploaded that kicks this function again.
-        if (sanitized) {
-          console.log('sanitized file was created, end function.');
-          return true;
-        }
+    // analyze image
+    console.log(`Analyzing ${fileName}.`);
+    const result = await visionClient.safeSearchDetection(
+      `gs://${bucketName}/${fileName}`
+    );
+    const [safeSearch] = result;
 
-        const bucket = storage.bucket(bucketName);
-        const tmpFilePathOriginal = path.join(
-          os.tmpdir(),
-          path.basename(fileName)
-        );
+    // blur the image if the image is offensive
+    if (
+      safeSearch.safeSearchAnnotation.adult === 'VERY_LIKELY' ||
+      safeSearch.safeSearchAnnotation.violence === 'VERY_LIKELY'
+    ) {
+      console.log(`The image ${file.name} has been detected as inappropriate.`);
+      return await blurImage(file);
+    }
 
-        const tmpFilePath1024w = path.join(
-          os.tmpdir(),
-          path.basename(`tmp1024w-${fileName}`)
-        );
-        const tmpFilePath192w = path.join(
-          os.tmpdir(),
-          path.basename(`tmp192w-${fileName}`)
-        );
-        const tmpFilePath96w = path.join(
-          os.tmpdir(),
-          path.basename(`tmp96w-${fileName}`)
-        );
+    console.log(`The image ${fileName} has been detected as OK.`);
+    return { sanitized: false };
+  } catch (err) {
+    console.error('failed image analyzation: ', err);
+    return false;
+  }
+};
 
-        return (
-          bucket
-            .file(fileName)
-            .download({
-              destination: tmpFilePathOriginal
-            })
-            // 1024w
-            .then(() =>
-              spawn('convert', [
-                tmpFilePathOriginal,
-                '-resize',
-                '1024x1024>',
-                '-quality',
-                '70',
-                tmpFilePath1024w
-              ])
-            )
-            .then(() =>
-              bucket.upload(tmpFilePath1024w, {
-                destination: `1024w/${path.basename(fileName)}`
-              })
-            )
+exports.onFileChange = functions
+  .region('asia-northeast1')
+  .storage.bucket('travelr-images')
+  .object()
+  .onFinalize(async object => {
+    try {
+      const bucketName = object.bucket;
+      const fileName = object.name;
 
-            // 192w
-            .then(() =>
-              spawn('convert', [
-                tmpFilePathOriginal,
-                '-resize',
-                '192x192w>',
-                '-quality',
-                '70',
-                tmpFilePath192w
-              ])
-            )
-            .then(() =>
-              bucket.upload(tmpFilePath192w, {
-                destination: `192w/${path.basename(fileName)}`
-              })
-            )
+      console.log('File upload detected!');
 
-            // 96w
-            .then(() =>
-              spawn('convert', [
-                tmpFilePathOriginal,
-                '-resize',
-                '96x96',
-                '-quality',
-                '70',
-                tmpFilePath96w
-              ])
-            )
-            .then(() =>
-              bucket.upload(tmpFilePath96w, {
-                destination: `96w/${path.basename(fileName)}`
-              })
-            )
+      if (
+        fileName.startsWith('1024w/') ||
+        fileName.startsWith('192w/') ||
+        fileName.startsWith('96w/')
+      ) {
+        console.log('Ignoreing already resized files!', fileName);
+        return true;
+      }
 
-            // complete
-            .then(() => console.log('resized files created!'))
-        );
-      })
-      .catch(err => console.error('an error occured!: ', err));
+      // check if the image is offensive and sanitize it if necessary
+      const { sanitized } = await blurOffensiveImages(object);
+
+      // do nothing if the image was sanitized.
+      // as the new image will be uploaded that kicks this function again.
+      if (sanitized) {
+        console.log('sanitized file was created, end function.');
+        return true;
+      }
+
+      // download the original file
+      const bucket = storage.bucket(bucketName);
+      const tmpFilePathOriginal = path.join(
+        os.tmpdir(),
+        path.basename(fileName)
+      );
+      await bucket.file(fileName).download({
+        destination: tmpFilePathOriginal
+      });
+
+      // 1024w
+      const tmpFilePath1024w = path.join(
+        os.tmpdir(),
+        `tmp1024w-${path.basename(fileName)}`
+      );
+      await spawn('convert', [
+        tmpFilePathOriginal,
+        '-resize',
+        '1024x1024>',
+        '-quality',
+        '70',
+        tmpFilePath1024w
+      ]);
+      await bucket.upload(tmpFilePath1024w, {
+        destination: `1024w/${path.basename(fileName)}`
+      });
+
+      // 192w
+      const tmpFilePath192w = path.join(
+        os.tmpdir(),
+        `tmp192w-${path.basename(fileName)}`
+      );
+      await spawn('convert', [
+        tmpFilePathOriginal,
+        '-resize',
+        '192x192w>',
+        '-quality',
+        '70',
+        tmpFilePath192w
+      ]);
+      await bucket.upload(tmpFilePath192w, {
+        destination: `192w/${path.basename(fileName)}`
+      });
+
+      // 96w
+      const tmpFilePath96w = path.join(
+        os.tmpdir(),
+        `tmp96w-${path.basename(fileName)}`
+      );
+      await spawn('convert', [
+        tmpFilePathOriginal,
+        '-resize',
+        '96x96',
+        '-quality',
+        '70',
+        tmpFilePath96w
+      ]);
+      await bucket.upload(tmpFilePath96w, {
+        destination: `96w/${path.basename(fileName)}`
+      });
+
+      // complete
+      console.log('resized files created!');
+      return true;
+    } catch (err) {
+      console.error('an error occured!: ', err);
+      return false;
+    }
   });
